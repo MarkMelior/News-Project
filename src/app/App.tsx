95bd93a1dda2b2c8d78fa792645097753b397c3f
MarkMelior <mark.melior@yandex.ru>
2024-01-26 15:58:30 +0300

    Первый UI Kit компонент

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,17 +1,16 @@
-import { Link } from 'react-router-dom'
 import './styles/index.scss'
 import { classNames } from 'shared/lib/classNames/classNames'
 import { useTheme } from './providers/ThemeProvider'
 import { AppRouter } from './providers/router'
+import { Navbar } from 'widgets/Navbar'
 
 const App = () => {
 	const { theme, toggleTheme } = useTheme()
 
 	return (
 		<div className={classNames('app', {}, [theme])}>
+			<Navbar></Navbar>
 			<button onClick={toggleTheme}>TOGGLE THEME</button>
-			<Link to={'/'}>Главная</Link>
-			<Link to={'/about'}>About</Link>
 			<AppRouter></AppRouter>
 		</div>
 	)
